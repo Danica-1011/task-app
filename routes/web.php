de5ffd7ac380f2ac025c993e4f5a9bb7428c9e92
Dgalinato123 <dgalinato123@gmail.com>
2026-05-05 10:58:36 +0800

    show welcome page on homepage

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/dashboard'); // 👈 changed this
+    return redirect('/welcome'); // 👈 changed this
 });
 
 Route::get('/dashboard', [TaskController::class, 'index'])
