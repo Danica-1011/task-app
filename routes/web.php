22dd819b411e154959f1e445a878ceabaaf819d4
Dgalinato123 <dgalinato123@gmail.com>
2026-05-05 11:04:44 +0800

    fix welcome route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/welcome'); // 👈 changed this
+    return view('welcome'); // ✅ fixed
 });
 
 Route::get('/dashboard', [TaskController::class, 'index'])
